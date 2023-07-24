4072fd33b5a939ae0fca5aca238b0646a2b9c789
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-07-24 14:18:50 -0400

    create new react setup

---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+export default function Todo() {
+    return (
+        <div>
+        <h1>Todo 1</h1>
+        </div>
+    )
+}
