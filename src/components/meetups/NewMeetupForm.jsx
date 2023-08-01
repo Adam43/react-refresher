38f4f94d1fd69e94fc7d1f385336c1f56734fde9
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-08-01 19:32:30 -0400

    sending a post http request

--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -3,7 +3,8 @@ import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+
+function NewMeetupForm(props) {
     const titleInputRef = useRef();
     const imageInputRef = useRef();
     const addressInputRef = useRef();
@@ -24,7 +25,7 @@ function NewMeetupForm() {
             description: enteredDescription,
         };
 
-        console.log(meetupData);
+        props.onAddMeetup(meetupData);
     }
 
   return (
--- src/pages/NewMeetups.jsx
+++ src/pages/NewMeetups.jsx
@@ -1,14 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-
-
 function NewMeetupsPage() {
-    return (
-        <section>
-            <h1>Add New Meetup</h1>
-            <NewMeetupForm />
-        </section>
-    )
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-209af-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+            "Content-Type": "application/json"
+        }
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
