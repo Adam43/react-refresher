c272b8725667aa1a99d274ce02e49a3531a35ecd
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-08-05 21:49:38 -0400

    added button link navigation

--- src/pages/NewMeetups.jsx
+++ src/pages/NewMeetups.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+    const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-209af-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +15,9 @@ function NewMeetupsPage() {
             "Content-Type": "application/json"
         }
       }
-    );
+    ).then(() => {
+        navigate("/");
+    });
   }
 
   return (
