c5ff93f0467eef062996eeb7f80dc144d033e857
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-08-07 16:57:33 -0400

    completed favoritesPage component

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AllMeetUpsPage from "./pages/AllMeetups.jsx";
 import NewMeetupsPage from "./pages/NewMeetups.jsx";
 import Favorites from "./pages/Favorites.jsx";
+import { FavoritesContextProvider } from "./store/Favorites-context.jsx";
 import App from "./App.jsx";
 import "./index.css";
-
 
 const router = createBrowserRouter([
   {
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
+  <FavoritesContextProvider>
     <RouterProvider router={router} />
-  
+  </FavoritesContextProvider>
 );
